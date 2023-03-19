836246564d865ec7f5779ff78a3e8ec1ee471ff0
YoungChulShin <go1323@gmail.com>
2023-03-19 21:40:07 +0900

    게이지 커스텀 메트릭 등록

--- actuator/src/main/java/study/spring/actuator/ActuatorApplication.java
+++ actuator/src/main/java/study/spring/actuator/ActuatorApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import study.spring.actuator.order.gauge.v1.StockConfigV1;
+import study.spring.actuator.order.gauge.v1.StockConfigV2;
 import study.spring.actuator.order.v0.OrderConfigV0;
 import study.spring.actuator.order.v1.OrderConfigV1;
 import study.spring.actuator.order.v2.OrderConfigV2;
@@ -14,7 +16,8 @@ import study.spring.actuator.order.v4.OrderConfigV4;
 //@Import(OrderConfigV1.class)
 //@Import(OrderConfigV2.class)
 //@Import(OrderConfigV3.class)
-@Import(OrderConfigV4.class)
+//@Import({OrderConfigV4.class, StockConfigV1.class})
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "study.spring.actuator.controller")
 public class ActuatorApplication {
 
